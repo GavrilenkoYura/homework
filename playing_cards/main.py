c72562e67642cf5f5161f76ca9ad1210dc206b43
yura <gshshaaha@gmail.com>
2023-12-14 00:08:36 +0300

    feat: update self.weight with logical operator or

--- playing_cards/main.py
+++ playing_cards/main.py
@@ -3,8 +3,7 @@ class Card:
     def __init__(self, suit, number, weight=None):
         self.suit = suit
         self.number = number
-        self.weight = weight if weight is not None else number
+        self.weight = weight or number
 
 
 one_card = Card('Bubna', 6)
-
