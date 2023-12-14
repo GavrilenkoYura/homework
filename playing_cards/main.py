ea487c70afd734edc2ca9f4fb8a0e0462fc05b51
yura <gshshaaha@gmail.com>
2023-12-14 18:26:11 +0300

    feat: add a value instead of a number in __init__ and add suit selection

--- playing_cards/main.py
+++ playing_cards/main.py
@@ -1,9 +1,14 @@
 class Card:
 
-    def __init__(self, suit, number=None, weight=None):
+    def __init__(self, suit, value, weight=None):
         self.suit = suit
-        self.number = number
-        self.weight = weight or number
+        self.value = value
+        if value in ['J', 'Q', 'K']:
+            self.weight = 10
+        elif value == 'A':
+            self.weight = weight or 1
+        else:
+            self.weight = weight or value
 
     def __add__(self, other):
         return self.weight + other.weight
@@ -15,11 +20,11 @@ class Card:
             'Clubs': '♣',
             'Spades': '♠'
         }
-        return (f" _________\n|{str(self.number).ljust(8)}{suits[self.suit]}|\n|         |\n|"
-                f"        |\n|{suits[self.suit]} {str(self.number).rjust(7)}|\n|_________|")
+        return (f" __________\n|{str(self.value).ljust(8)}{suits[self.suit]}|\n|         |\n|"
+                f"         |\n|{suits[self.suit]} {str(self.value).rjust(7)}|\n __________\n")
 
 
-one_card = Card('Hearts', 6)
+one_card = Card('Hearts', 'A')
 second_card = Card('Spades', 9)
 print(one_card + second_card)
 print(one_card)
