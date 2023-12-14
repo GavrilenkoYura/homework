e70cb86801db28117fb2512b3895bafe69674e8d
yura <gshshaaha@gmail.com>
2023-12-14 18:48:31 +0300

    feat: add create_deck()

--- playing_cards/main.py
+++ playing_cards/main.py
@@ -1,3 +1,6 @@
+from random import choice
+
+
 class Card:
 
     def __init__(self, suit, value, weight=None):
@@ -17,15 +20,32 @@ class Card:
             'Clubs': '♣',
             'Spades': '♠'
         }
-        return (f" __________\n|{str(self.value).ljust(8)}{suits[self.suit]}|\n|         |\n|"
-                f"         |\n|{suits[self.suit]} {str(self.value).rjust(7)}|\n __________\n")
+        return (f" _________ \n|{str(self.value).ljust(8)}{suits[self.suit]}|\n|         |\n|"
+                f"         |\n|{suits[self.suit]} {str(self.value).rjust(7)}|\n _________ \n")
 
 
 def add_cards(card1, card2):
     return card1.weight + card2.weight
 
 
+def create_deck():
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    deck = []
+
+    for _ in range(6):
+        suit = choice(suits)
+        value = choice(values)
+        card = Card(suit, value)
+        deck.append(card)
+
+    return deck
+
+
 first_card = Card('Hearts', 'A', 11)
 second_card = Card('Spades', 9)
 result = add_cards(first_card, second_card)
 print(result)
+
+first_deck = create_deck()
+print(*first_deck, sep='')
