a02c62cdfc6c7466151c59f8b06ed098c8bb732c
yura <gshshaaha@gmail.com>
2023-12-14 18:06:59 +0300

    feat: add __repr__ to display cards

--- playing_cards/main.py
+++ playing_cards/main.py
@@ -1,6 +1,6 @@
 class Card:
 
-    def __init__(self, suit, number, weight=None):
+    def __init__(self, suit, number=None, weight=None):
         self.suit = suit
         self.number = number
         self.weight = weight or number
@@ -8,7 +8,18 @@ class Card:
     def __add__(self, other):
         return self.weight + other.weight
 
+    def __repr__(self):
+        suits = {
+            'Hearts': '♥',
+            'Diamonds': '♦',
+            'Clubs': '♣',
+            'Spades': '♠'
+        }
+        return (f" _________\n|{str(self.number).ljust(8)}{suits[self.suit]}|\n|         |\n|"
+                f"        |\n|{suits[self.suit]} {str(self.number).rjust(7)}|\n|_________|")
 
-one_card = Card('Bubna', 6)
-second_card = Card('Chervi', 9)
+
+one_card = Card('Hearts', 6)
+second_card = Card('Spades', 9)
 print(one_card + second_card)
+print(one_card)
