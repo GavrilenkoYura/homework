43aae20fd48178b43fdd54d9149c9a7ac9121283
yura <gshshaaha@gmail.com>
2023-12-14 20:02:36 +0300

    feat: make add_cards() and created_deck() static methods instead of functions

--- playing_cards/main.py
+++ playing_cards/main.py
@@ -23,29 +23,29 @@ class Card:
         return (f" _________ \n|{str(self.value).ljust(8)}{suits[self.suit]}|\n|         |\n|"
                 f"         |\n|{suits[self.suit]} {str(self.value).rjust(7)}|\n _________ \n")
 
+    @staticmethod
+    def add_cards(card1, card2) -> int:
+        return card1.weight + card2.weight
 
-def add_cards(card1: Card, card2: Card) -> int:
-    return card1.weight + card2.weight
+    @staticmethod
+    def create_deck() -> list:
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+        deck = []
 
+        for _ in range(6):
+            suit = choice(suits)
+            value = choice(values)
+            card = Card(suit, value)
+            deck.append(card)
 
-def create_deck() -> list:
-    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
-    deck = []
-
-    for _ in range(6):
-        suit = choice(suits)
-        value = choice(values)
-        card = Card(suit, value)
-        deck.append(card)
-
-    return deck
+        return deck
 
 
 first_card = Card('Hearts', 'A', 11)
 second_card = Card('Spades', 9)
-result = add_cards(first_card, second_card)
+result = Card.add_cards(first_card, second_card)
 print(result)
 
-first_deck = create_deck()
+first_deck = Card.create_deck()
 print(*first_deck, sep='')
