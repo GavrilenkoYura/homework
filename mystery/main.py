cff4c21784aae7e76c9e1985dc0f9f695fdae5a0
yura <gshshaaha@gmail.com>
2023-12-17 17:18:22 +0300

    feat: add more quizes, point system

--- mystery/main.py
+++ mystery/main.py
@@ -23,13 +23,61 @@ class Mystery:
             return 0
 
 
-q1 = Mystery(question="Откуда на Беларусь готовилось нападение?",
-             answer="там 4 позиции, я карту принёс, сейчас покажу",
-             choices=[
-                 "Откуда мне знать?",
-                 "Со склада грязи.",
-                 "С планеты Нибиру.",
-                 "С загнивающег запада."
-             ])
+questions = [
+    Mystery(question="What is the capital of Myanmar?",
+            answer="Naypyidaw",
+            choices=[
+                "Thimpu",
+                "Sana",
+                "Nikosia",
+                "Vindhuk"
+            ]),
+    Mystery(question="What does HTTP stand for?",
+            answer="HyperText Transfer Protocol",
+            choices=[
+                "HyperText Transfer Pillow",
+                "HyperText Transmission Protocol",
+                "HyperText Transmission Protocol",
+                "HyperText Transfer Plague"
+            ]),
+    Mystery(question="What does SQL stand for?",
+            answer="Structured Query Language",
+            choices=[
+                "Stylish Question Language",
+                "Stylish Query Language",
+                "Statement Question Language",
+                "Structured Query Library"
+            ]),
+    Mystery(question="What is the derivative of x^2?",
+            answer="2x",
+            choices=[
+                "2x^2",
+                "x^2",
+                "2",
+                "x"
+            ]),
+    Mystery(question="What is the integral of x^2?",
+            answer="1/3*x^3",
+            choices=[
+                "1/3*x^2",
+                "2x",
+                "x^2",
+                "x"
+            ]),
+    Mystery(question="What is the sum of the first 10 natural numbers?",
+            answer="55",
+            choices=[
+                "44",
+                "60",
+                "50",
+                "45"
+            ])
+]
 
-print(q1.quiz())
+shuffle(questions)
+score = 0
+for question in questions:
+    score += question.quiz()
+    print('\n————————————————————————————————————————————————————————————————\n')
+
+print(f"Your score is: {score}. You could have gotten {len(question) * 3} points")
