b4a009c88285fee64188da2286b1d931ce18fc20
yura <gshshaaha@gmail.com>
2024-01-08 00:08:09 +0300

    feat: add class Player

--- maze/main.py
+++ maze/main.py
@@ -20,3 +20,19 @@ class Door:
             return self.second_room
         elif current_room == self.second_room:
             return self.first_room
+
+class Player:
+    def __init__(self, name, start_room):
+        self.name = name
+        self.curr_room = start_room
+
+    def look_around(self):
+        doors = self.curr_room.view_all_doors()
+        print(f'You are in {self.curr_room.name}.')
+        print('The are doors leading to the following rooms:')
+        for door in doors:
+            print(door)
+
+    def enter_room(self, door):
+        self.curr_room = door.get_next_room(self.curr_room)
+        self.look_around()
